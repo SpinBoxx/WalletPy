dbdcd9e8c63ebda1954e7e96cb9d9d45034e5e92
Lois VIAUD <lois.viaud@capgemini.com>
2022-03-10 10:40:01 +0100

    change some things

--- WalletPy/WalletPy/views.py
+++ WalletPy/WalletPy/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import  render, redirect
 import requests
 
 def homepage(request):
-    test = requests.get("https://api.coingecko.com/api/v3/coins/bitcoin")
-    print(test.json())
+    """ test = requests.get("https://api.coingecko.com/api/v3/coins/bitcoin")
+    print(test.json()) """
     return render (request=request, template_name="homepage.html")
 
 def dashboard(request):
