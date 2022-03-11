d994f7a96f07026006869482d8fde97eab915ff4
Nicolas <nicobaca17@gmail.com>
2022-03-11 16:36:20 +0100

    Update password, username

--- WalletPy/account/views.py
+++ WalletPy/account/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def index(request):
-    return render(request, 'account.html')
+    user = ''
+    context = {user : request.user}
+    return render(request, 'account.html', context)
+
+def update_user(request):
+    context = {}
+    if request.method == 'POST':
+
+        password = request.POST['new_password']
+        username = request.POST['username']
+
+        
+        request.user.set_password(password)
+        request.user.username = username
+        request.user.save()
+
+    return redirect('/profile', context)
+        
 
